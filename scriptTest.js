b391174eb5952edbea526d35eae0a5dc5e0ee77c
Tom Baty <tbaty@myges.fr>
2023-11-05 17:24:55 +0100

    scriptTest

---
+++ scriptTest.js
@@ -0,0 +1,26 @@
+// Déclaration de la fonction d'affichage
+function afficherBonjour() {
+  console.log("Bonjour, monde !");
+}
+
+// Appel de la fonction
+afficherBonjour();
+
+// Déclaration d'une variable contenant un message
+let message = "C'est une journée ensoleillée.";
+
+// Affichage du message
+console.log(message);
+
+// Une boucle simple
+for (let i = 0; i < 5; i++) {
+  console.log("Itération " + (i + 1));
+}
+
+// Déclaration d'un objet
+let personne = { nom: "Dupont", prénom: "Jean", âge: 30 };
+
+// Affichage des propriétés de l'objet
+console.log("Nom: " + personne.nom);
+console.log("Prénom: " + personne.prénom);
+console.log("Âge: " + personne.âge);
