47ef0abaca449965ab3583ef27dbfd44780c99b2
Tom Baty <tbaty@myges.fr>
2023-11-05 17:13:40 +0100

    2 eme test

---
+++ script2.js
@@ -0,0 +1,10 @@
+function calculer(a,b){
+let somme=a+b
+if(somme>10){
+console.log('La somme est supérieure à 10')
+}else{console.log('La somme est inférieure à 10')
+}
+return somme
+}
+let resultat = calculer(5,3)
+console.log(resultat)
